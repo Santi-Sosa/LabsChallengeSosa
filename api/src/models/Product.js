99479f4c644a29288d4a956ab14a7f362b198b39
Santi-Sosa <santisose01@gmail.com>
2020-09-30 07:54:56 -0300

    Upgrade

---
+++ api/src/models/Product.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('product', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  });
+  sequelize.define('image', {
+    source: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+ });
+  sequelize.define('categories', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
+};
+
+
